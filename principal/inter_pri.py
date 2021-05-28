f96164da80ca861676bae53b260d3caf610938ef
Celot1979 <zatak631979@gmail.com>
2021-05-28 17:45:13 +0200

    ordenamiento elementos con frame

--- principal/inter_pri.py
+++ principal/inter_pri.py
@@ -2,8 +2,26 @@ from tkinter import *
 """Creación de un frame para poder ubicar mejor los elementos de la ventana principal """
 def principal(self):
     self.nombre = StringVar()
-    self.etiqueta_nom = Label(self.frame, text="Nombre",)
+    self.contrasena = StringVar()
+
+    self.etiqueta_nom = Label(self.frame, text="NOMBRE")
     self.etiqueta_nom.grid(row= 1, column=1)
+    self.etiqueta_con = Label(self.frame, text="CONTRASEÑA")
+    self.etiqueta_con.grid(row= 2, column=1)
 
     self.entrada_nom= Entry(self.frame, textvariable= self.nombre)
     self.entrada_nom.grid(row= 1, column=2)
+    self.entrada_con= Entry(self.frame, textvariable= self.contrasena)
+    self.entrada_con.grid(row= 2,column=2)
+
+    self.boton1 = Button(self.frame, text="CONECTAR")
+    self.boton1.grid(row=3,column=1, padx= 10, pady= 10)
+    self.boton2 = Button(self.frame, text="SALIR")
+    self.boton2.grid(row=3,column=2)
+    self.boton3 = Button(self.frame, text="REGISTRAR")
+    self.boton3.grid(row=3,column=3)
+
+    self.imagen = PhotoImage(file="Src/imag/Proctetora.png")
+    self.Imagen2= Label(self.frame, image=self.imagen)
+    self.Imagen2.grid(row=4,columnspan=5)
+
--- principal/star.py
+++ principal/star.py
@@ -5,10 +5,13 @@ class Protectora:
     def __init__(self,ventana):
         self.ventana = ventana
         self.frame = Frame(self.ventana)
-        self.frame.pack()
-        self.frame.config(bg="lightblue")   
+        self.frame.pack(expand=1)
+        #self.frame.config(bg="lightblue")   
         self.frame.config(width=1200,height=5000) 
         self.frame= principal(self)
+
+        
+
 
 
 Proyecto = Protectora(raiz)
