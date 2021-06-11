8a18c6ac2b4f389c01d5c8486da7901fe05d0b51
Celot1979 <zatak631979@gmail.com>
2021-06-11 18:12:36 +0200

    Revision

---
+++ principal/tools.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+def perro():
+    imagen = PhotoImage(file="src/imag/Perro 2.png")
+    Imagen2= Label(self.frame_dos, image=imagen)
+    Imagen2.grid(row=4,columnspan=6)
+
+
+"""self.imagen = PhotoImage(file="src/imag/Perro 2.png")
+self.Imagen2= Label(self.frame_dos, image=self.imagen)
+self.Imagen2.grid(row=4,columnspan=6)"""
